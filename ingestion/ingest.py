83a39b41f7b872e998d1d6f36931baff62ce1c0a
Albuqr <jpcaastroo@gmail.com>
2026-04-09 20:13:02 -0300

    Updated ingestion.py, bronze transactions budget and equipment all pushed to bigquery, prod plan and maint logs still not finished

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,28 +1,39 @@
 import pandas as pd
 from google.cloud import bigquery
+from dotenv import load_dotenv
+import os
+import re
+from unidecode import unidecode
 
-client = bigquery.Client.from_service_account_json('./gcp-credentials.json')
+load_dotenv()
+client = bigquery.Client.from_service_account_json(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
 
 job_config = bigquery.LoadJobConfig(
+    write_disposition="WRITE_TRUNCATE",
     time_partitioning=bigquery.TimePartitioning(
         field="ingestion_date"
     )
 )
 
 def ingest_transactions_fn():
-    df = pd.read_excel("./data/raw/bronze_transactions.xlsx")
+    df = pd.read_excel("./data/raw/Brumelli.xlsx", sheet_name="DFC")
+
     df["ingestion_date"] = pd.Timestamp.now()
     table_id = "factory-lakehouse.factory_lakehouse.bronze_transactions"
     client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
 def ingest_budget_fn():
-    df = pd.read_excel("./data/raw/bronze_budget.xlsx")
+    df = pd.read_excel("./data/raw/Brumelli.xlsx", sheet_name="DRE", header=None)
+    df.columns = df.columns.astype(str)
+    cols_to_convert = [c for c in df.columns if c != "ingestion_date"]
+    df[cols_to_convert] = df[cols_to_convert].astype(str)
     df["ingestion_date"] = pd.Timestamp.now()
     table_id = "factory-lakehouse.factory_lakehouse.bronze_budget"
     client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
 def ingest_equipment_fn():
-    df = pd.read_excel("./data/raw/bronze_equipment.xlsx")
+    df = pd.read_excel("./data/raw/Inventario.xlsx")
+    df.columns = [re.sub(r'[^a-z0-9_]', '_', unidecode(col).lower().strip()) for col in df.columns]
     df["ingestion_date"] = pd.Timestamp.now()
     table_id = "factory-lakehouse.factory_lakehouse.bronze_equipment"
     client.load_table_from_dataframe(df, table_id, job_config=job_config)
