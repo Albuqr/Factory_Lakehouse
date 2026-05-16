363ed58c851eeabd74eec186d2775376dcc5fac6
Albuqr <jpcaastroo@gmail.com>
2026-05-16 01:32:21 -0300

    Added a middleware layer with an api to output the data for the data platform, repo 3 of this project

---
+++ middleware/main.py
@@ -0,0 +1,60 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from dashboard.config import client, PROJECT_ID, DATASET_ID
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/budget-variance")
+def budget_variance():
+    query = """
+        select cost_center, budget_amount_brl, month_key
+
+        from  
+
+        `factory-lakehouse.factory_lakehouse.silver_budget`
+            where month_key = (
+            SELECT max(month_key) FROM `factory-lakehouse.factory_lakehouse.silver_budget`)
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    data = [{**dict(row.items()), "variance_brl": None, "variance_pct": None, "status_flag": None} for row in results]
+
+    return {"data": data}
+
+@app.get("/api/equipment_status")
+def equipment_status():
+    query = """
+        select *
+        
+        from
+
+        `factory-lakehouse.factory_lakehouse.gold_equipment_status`
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    data = [dict(row.items()) for row in results]
+
+    return {"data": data}
+
+
+@app.get("/api/sku_economics")
+def sku_economics():
+    query = """
+        select *
+
+        from
+
+        `factory-lakehouse.factory_lakehouse.gold_cost_per_unit`
+        where month_key = (
+            SELECT max(month_key) FROM `factory-lakehouse.factory_lakehouse.gold_cost_per_unit`)
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    data = [dict(row.items()) for row in results]
+
+    return {"data": data}
